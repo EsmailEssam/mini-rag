4d1651d37b9a7d8aac5e8a85d0b963fd0b78b898
Esmail <esmailessam6@gmail.com>
2025-09-09 11:35:06 +0300

    set indecies for project and chunks

--- src/models/db_schemas/project.py
+++ src/models/db_schemas/project.py
@@ -15,3 +15,16 @@ class Project(BaseModel):
     class Config:
         # allow ObjectId to be used as a field
         arbitrary_types_allowed = True
+    
+    @classmethod
+    def get_indexes(cls):
+        return [
+            {
+                'key':[
+                    ('project_id', 1)
+                ],
+                'name': 'project_id_index_1',
+                'unique': True
+            }
+        ]
+
--- src/routes/data.py
+++ src/routes/data.py
@@ -24,7 +24,7 @@ data_router = APIRouter(
 async def upload_data(request: Request, project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
     
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
 
@@ -85,7 +85,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
     overlap_size = process_request.overlap_size
     do_reset = process_request.de_reset
 
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
 
@@ -93,7 +93,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
         project_id=project_id
     )
 
-    chunk_model = ChunkModel(
+    chunk_model = await ChunkModel.create_instance(
         db_client=request.app.db_client
     )
 
